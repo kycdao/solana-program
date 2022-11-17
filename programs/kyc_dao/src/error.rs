945939a6f85d019df35b67c84f6ea734a773745e
GUILHERME NEVES <asguilift@hotmail.com>
2022-11-16 21:50:18 -0300

    fix: spelling

--- programs/kyc_dao/src/error.rs
+++ programs/kyc_dao/src/error.rs
@@ -5,7 +5,7 @@ pub enum ErrorCode {
     #[msg("SECP256K1 program or lenght doesn't match")]
     InvalidDataProvided,
 
-    #[msg("The signature data provded to validate the metadata is incorrect")]
+    #[msg("The signature data provided to validate the metadata is incorrect")]
     SignatureVerificationFailed,
 
     #[msg("You don't have enough SOL to mint this NFT")]
