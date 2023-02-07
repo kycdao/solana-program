2b20fdd6af58529398617dfb51eef278c5213806
Jasper Timm <jaspa@jaspa.codes>
2023-02-07 14:38:48 +0000

    A few more renames

--- tests/cpiExample/cpiExample.ts
+++ tests/cpiExample/cpiExample.ts
@@ -10,7 +10,7 @@ import {
     sendAndConfirmTransaction,
   } from '@solana/web3.js'
   import { Ntnft } from '../../target/types/ntnft'
-  import { KycdaoCpiExample } from '../../target/types/kycdao_cpi_example'
+  import { NtnftCpiExample } from '../../target/types/ntnft_cpi_example'
   import {
     KYCDAO_PROGRAM_ID
   } from '../../utils/constants'
@@ -25,7 +25,7 @@ import {
 
 const { SystemProgram } = web3
 
-let exampleProgram: Program<KycdaoCpiExample>
+let exampleProgram: Program<NtnftCpiExample>
 let kycdaoProgram: Program<Ntnft>
 
 describe('tests', () => {
@@ -37,73 +37,61 @@ describe('tests', () => {
   }),
 
   it('Should check the checkAddress function', async () => {
-    try {
-      console.log("Running checkAddress test...")
+    console.log("Running checkAddress test...")
 
-      exampleProgram = workspace.KycdaoCpiExample as Program<KycdaoCpiExample>
-      kycdaoProgram = workspace.Ntnft as Program<Ntnft>
-  
-      const [statusId, _bump] = await getStatusId(RECEIVER_WALLET.publicKey)
+    exampleProgram = workspace.NtnftCpiExample as Program<NtnftCpiExample>
+    kycdaoProgram = workspace.Ntnft as Program<Ntnft>
 
-      // Run checkAddress to check if the user has a valid token
-      const reqAcctsCheckAddress = {
-        kycdaoStatus: statusId,
-        kycdaoProgram: KYCDAO_PROGRAM_ID,
-      }
+    const [statusId, _bump] = await getStatusId(RECEIVER_WALLET.publicKey)
 
-      const resp = await exampleProgram.methods.checkAddress(
-        RECEIVER_WALLET.publicKey,
-      )
-      .accounts(reqAcctsCheckAddress)
-      .view()
-
-      console.log(`checkAddress resp: ${resp}`)
-
-    } catch (err) {
-        console.log(err)
+    // Run checkAddress to check if the user has a valid token
+    const reqAcctsCheckAddress = {
+      kycdaoStatus: statusId,
+      kycdaoProgram: KYCDAO_PROGRAM_ID,
     }
+
+    const resp = await exampleProgram.methods.checkAddress(
+      RECEIVER_WALLET.publicKey,
+    )
+    .accounts(reqAcctsCheckAddress)
+    .view()
+
+    console.log(`checkAddress resp: ${resp}`)
   })
 
   it('Should add a new member after checking kycDAO status', async () => {
-    try {
-      console.log("Running addNewMember test...")
+    const [statusId, _bump] = await getStatusId(RECEIVER_WALLET.publicKey)
 
-      const [statusId, _bump] = await getStatusId(RECEIVER_WALLET.publicKey)
+    // Generate a random byte array for the seed
+    const membershipSeed = ethers.utils.randomBytes(8)
 
-      // Generate a random byte array for the seed
-      const membershipSeed = ethers.utils.randomBytes(8)
+    const [membershipStatusId, _] = await web3.PublicKey.findProgramAddress(
+      [membershipSeed, RECEIVER_WALLET.publicKey.toBuffer()],
+      exampleProgram.programId,
+    )
 
-      const [membershipStatusId, _] = await web3.PublicKey.findProgramAddress(
-        [membershipSeed, RECEIVER_WALLET.publicKey.toBuffer()],
-        exampleProgram.programId,
-      )
-
-      // Run addNewMember to add new member
-      const reqAcctsAddNewMember = {
-        receiver: RECEIVER_WALLET.publicKey,
-        kycdaoProgram: kycdaoProgram.programId,
-        kycdaoStatus: statusId,
-        membershipStatus: membershipStatusId,
-        systemProgram: SystemProgram.programId,
-      }
-
-      // This membership seed is just to ensure we are using a new account for each test run
-      const tx = await exampleProgram.methods.addNewMember(new BN(1), Buffer.from(membershipSeed))
-      .accounts(reqAcctsAddNewMember)
-      .transaction()
-
-      const txResp = await sendAndConfirmTransaction(
-        AnchorProvider.env().connection,
-        tx,
-        [RECEIVER_WALLET],
-      )
-
-      const log = await getLogIncluding('', exampleProgram, txResp)
-      console.log(`addNewMember log: ${log}`)
-
-    } catch (err) {
-        console.log(err)
+    // Run addNewMember to add new member
+    const reqAcctsAddNewMember = {
+      receiver: RECEIVER_WALLET.publicKey,
+      kycdaoProgram: kycdaoProgram.programId,
+      kycdaoStatus: statusId,
+      membershipStatus: membershipStatusId,
+      systemProgram: SystemProgram.programId,
     }
+
+    // This membership seed is just to ensure we are using a new account for each test run
+    const tx = await exampleProgram.methods.addNewMember(new BN(1), Buffer.from(membershipSeed))
+    .accounts(reqAcctsAddNewMember)
+    .transaction()
+
+    const txResp = await sendAndConfirmTransaction(
+      AnchorProvider.env().connection,
+      tx,
+      [RECEIVER_WALLET],
+    )
+
+    const log = await getLogIncluding('', exampleProgram, txResp)
+    console.log(`addNewMember log: ${log}`)
   })
 
 })
